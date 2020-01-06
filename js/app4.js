12245cc9e86a879d4b1b6c6277e0ff0976023ecf
Josh Doyle <me@joshdoyle.us>
2020-01-06 16:48:22 -0600

    fixed all the id issues

--- js/app4.js
+++ js/app4.js
@@ -25,8 +25,10 @@ class Game {
 		let boardRow = []
 		let col = 0
 
-		$('.square').each(function(i, obj) {
-	
+
+		$('.square').each(function(obj) {
+			//add id to div. Needed to find div position in array.
+			let id = $(this).attr('id')
 			let status = $(this).children().attr('class')
 
 			if(status === undefined) {
@@ -37,39 +39,140 @@ class Game {
 		    	status = BLOCKED
 		    }	
 
-		    if(col < BOARD_COLUMNS - 1) {
-		    	boardRow.push(status)
-		    	col++
-		    } else if (col = BOARD_COLUMNS - 1) {
-		    	boardRow.push(status)
-		    	col = 0
+
+		    if(col === BOARD_COLUMNS - 1) {
+			    boardRow.push({id, status})	    	
 		    	board.push(boardRow)
+		    	col = 0
 		    	boardRow = []
+		    } else {
+		    	boardRow.push({id, status})
+		    	col++
 		    }	
 
 		}) 
+		console.log(board)
 	return board
 }
+
+	findCurrentSquareLocation(id){
+		debugger
+		for(let row = 0; row < BOARD_ROWS; row++){
+			for(let col = 0; col < BOARD_COLUMNS; col++){
+				if(this.board[row][col].id === id){
+					return {id, row, col}
+				}
+			}
+		}
+	}
+
+	lookUp(currentSquare){
+		let destinationSquare
+		
+		let loc = this.findCurrentSquareLocation(currentSquare)
+		let startingRow = loc.row - 1
+		let col = loc.col // the column of the current location
+		for(let row = startingRow; row >= 0; row--){
+			if(this.board[row][col].status != BLOCKED){
+				destinationSquare = this.board[row][col].id
+			} else {
+				break
+			}
+		}
+		return destinationSquare
+	}
+
+	lookDown(currentSquare){
+		let destinationSquare
+		
+		let loc = this.findCurrentSquareLocation(currentSquare)
+		let startingRow = loc.row + 1
+		let col = loc.col // the column of the current location
+		for(let row = startingRow; row >= 0; row++){
+			if(this.board[row][col].status != BLOCKED){
+				destinationSquare = this.board[row][col].id
+			} else {
+				break
+			}
+		}
+		return destinationSquare
+	}
+
+
+	lookLeft(currentSquare){
+		let destinationSquare
+		
+		let loc = this.findCurrentSquareLocation(currentSquare)
+		let startingCol = loc.col - 1
+		let row = loc.row
+		for(let col = startingCol; col >= 0; col--){
+			if(this.board[row][col].status != BLOCKED){
+				destinationSquare = this.board[row][col].id
+			} else {
+				break
+			}
+		}
+		return destinationSquare
+	}
+
+	lookRight(currentSquare){
 	
-	moveAnimate(element, newParent){
+		let destinationSquare
+		
+		let loc = this.findCurrentSquareLocation(currentSquare)
+		let startingCol = loc.col + 1
+		let row = loc.row
+		for(let col = startingCol; col >= 0; col++){
+			if(this.board[row][col].status != BLOCKED){
+				destinationSquare = this.board[row][col].id
+			} else {
+				break
+			}
+		}
+		return destinationSquare
+	}
+	
+	moveUp(tokenId){
+		debugger
+		// get parent of token
+		let origin = $(tokenId).parent().attr("id")
+
+		// let origin = this.findCurrentSquareLocation(tokenId).id
+		let destination = this.lookUp(origin)
+
+		origin = '#' + origin
+		destination = '#' + destination
+
+		this.moveToken(tokenId, destination, origin)
+
+	}
+
+	moveToken(token, newParent, oldParent){
+		
 	    //Allow passing in either a jQuery object or selector
-	    element = $(element);
+	    // debugger
+	    token = $(token);
 	    newParent= $(newParent);
+	    oldParent = $(oldParent)
+	    // debugger
+	    let oldOffset = token.offset();
+	    // let oldPosition = token.position()
+	    // console.log('oldOffset', oldOffset)
+	    // console.log('old Position',element.position())
+	    token.appendTo(newParent);
+	    let newPosition = token.offset();
 
-	    var oldOffset = element.offset();
-	    element.appendTo(newParent);
-	    var newOffset = element.offset();
-
-	    var temp = element.clone().appendTo('body');
+	    let temp = token.clone().appendTo(oldParent);
 	    temp.css({
-	        'position': 'absolute',
-	        'left': oldOffset.left,
-	        'top': oldOffset.top,
-	        'z-index': 1000
+	        // 'position': 'relative',
+	        // 'left': oldPosition.left,
+	        // 'top': oldPosition.top,
+	        // 'z-index': 1000
 	    });
-	    element.hide();
-	    temp.animate({'top': newOffset.top, 'left': newOffset.left}, 'slow', 'easeOutBounce', function(){
-	       element.show();
+	    // debugger
+	    token.hide();
+	    temp.animate({'top': newPosition.top, 'left': newPosition.left}, 500, 'swing', function(){
+	       token.show();
 	       temp.remove();
     });
 }
@@ -81,14 +184,14 @@ myGame.buildBoard()
 
 
 $( "#game-board" ).focus(function() {
-  console.log('event fired')
+  // console.log('event fired')
 });
 
 
 $('#game-board').on('click', (event) => {
   // so it must be converted back to jQuery
   const $etarget = $(event.target)
-  console.log($etarget)
+  // console.log($etarget)
 })
 
 
@@ -106,11 +209,11 @@ $(document).keydown(function(e){
 		$('#temp').remove();
 		$('<div id="temp"></div>').css('margin-left', '50px').prependTo($('body'))
 	});
-	console.log('key press')
+	// console.log('key press')
 	let t = myGame.currentTarget
     switch (e.which){
     case 37:    //left arrow key
-    	console.log('left arrow')
+    	// console.log('left arrow')
     	$( " #11 " ).animate({ "left": "-=50px" }, "fast" );
         
         break;
